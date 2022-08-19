19d43003da5f23ada650f990f3b2f45ef2107b66
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-08-19 10:11:28 -0700

    remove validation for testing in Postman

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -5,13 +5,13 @@ namespace AnimalShelter.Models
     public class Animal
     {
         public int AnimalId { get; set; }
-        [Required]
-        [StringLength(20)]
+        // [Required]
+        // [StringLength(20)]
         public string Name { get; set; }
-        [Required]
+        // [Required]
         public string Species { get; set; }
-        [Required]
-        [Range(0, 200, ErrorMessage = "Age must be between 0 and 200.")]
+        // [Required]
+        // [Range(0, 200, ErrorMessage = "Age must be between 0 and 200.")]
         public int Age { get; set; }
        
     }
