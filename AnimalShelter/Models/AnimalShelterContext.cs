57f7ca0e6d40b9d16242523745c0a683f3af834d
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-08-19 09:22:54 -0700

    update Animal.cs with properties and create Context class with seed data

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+{
+  builder.Entity<Animal>()
+      .HasData(
+          new Animal { AnimalId = 1, Name = "Daisy", Species = "Dog", Age = 1},
+          new Animal { AnimalId = 2, Name = "Spot", Species = "Cat", Age = 2},
+          new Animal { AnimalId = 3, Name = "Max", Species = "Dog", Age = 3},
+          new Animal { AnimalId = 4, Name = "Pepper", Species = "Dog", Age = 4},
+          new Animal { AnimalId = 5, Name = "Penelope", Species = "Cat", Age = 15}
+      );
+}
+    }
+}
